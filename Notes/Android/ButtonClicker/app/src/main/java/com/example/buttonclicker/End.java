93a47961a4d88c6692e2fbda2710d871555fc302
Ben <2smallgarage@gmail.com>
2023-11-16 12:46:17 -0600

    ButtonClicker update

--- Notes/Android/ButtonClicker/app/src/main/java/com/example/buttonclicker/End.java
+++ Notes/Android/ButtonClicker/app/src/main/java/com/example/buttonclicker/End.java
@@ -14,30 +14,33 @@ public class End extends AppCompatActivity {
     //THE ENDING THINGY
     private EditText leave;
     private Button check;
-    boolean correct = false;
-    private String answer;
+    private Boolean correct;
+    private String answer, finalText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end);
-
+        correct = false;
         leave = findViewById(R.id.txtPassword);
         check = findViewById(R.id.btnCheck);
-
 
         check.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 answer = String.valueOf(leave.getText());
+                Intent i;
                 if(answer.equals("Taylor")) {
-                    
-
+                    correct = true;
+                    finalText = "You've guessed correctly, and survived with the bag of milk.";
+                    i = new Intent(End.this, MainActivity.class);
+                    i.putExtra("correct", correct);
+                    i.putExtra("finalText" , finalText);
                 } else {
-                    Intent i = new Intent(End.this, Sleep.class);
-                    startActivity(i);
+                    i = new Intent(End.this, Sleep.class);
                 }
+                startActivity(i);
             }
         });
     }
--- Notes/Android/ButtonClicker/app/src/main/java/com/example/buttonclicker/MainActivity.java
+++ Notes/Android/ButtonClicker/app/src/main/java/com/example/buttonclicker/MainActivity.java
@@ -6,18 +6,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button start;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        boolean correct = getIntent().getBooleanExtra("correct",false);
+        String finalText = getIntent().getStringExtra("finalText");
 
-        start = findViewById(R.id.btnStart);
+        Button start = findViewById(R.id.btnStart);
+        TextView endText = findViewById(R.id.txtEnd);
+
+        if(correct) {
+            assert finalText != null;
+            endText.setText(String.format(finalText));
+        }
 
         start.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,7 +35,5 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
-
-
     }
 }
